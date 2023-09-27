f2b88cbf7bb3cf94ffba93c39c0e6ef761c3d590
shawn edgell <shawn_edgell@hotmail.com>
2023-09-26 23:09:11 -0400

    updated grid layout and added proper dimensions

--- sketch.js
+++ sketch.js
@@ -1,30 +1,7 @@
-const containerEl = document.querySelector('.container');
+let i=0, n=16;
 
-function createDivs(num) {
-  // Check if the input is a positive integer
-  if (Number.isInteger(num) && num > 0) {
-    for (let i = 0; i < num; i++) {
-      // Create a new div element
-      const div = document.createElement('div');
-      
-      // Add a CSS class to the div
-      div.classList.add('my-div');
-
-      // Append the div to the container element
-      containerEl.appendChild(div);
-    }
-  } else {
-    console.log('Please provide a positive integer as the parameter.');
-  }
-}
-
-const numOfDivs = 256;
-createDivs(numOfDivs);
-
-
-
-
-
-
+container.innerHTML = 
+    `<div class="row">${'<div class="cell">X</div>'.repeat(n)}</div>`
+    .repeat(n).replace(/X/g,_=> (i++).toString(n) )
 
 
